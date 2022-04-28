43bceb268a7aa4dd4b5753a48a5c2101cd098e69
Polyana Albanaz <102770020+Itspolyana@users.noreply.github.com>
2022-04-28 08:23:58 -0300

    Add files via upload

---
+++ prg_atividade_27_04/atividade 2.py
@@ -0,0 +1,5 @@
+nome  = input("Qual é o seu nome?")
+idade = input("Qual sua idade ?")
+peso = input("Qual o seu peso ?")
+
+print(f"O nome do entrevistado é: {nome}, ele tem {idade} anos, e possui {peso} kg") 
---
+++ prg_atividade_27_04/atividade.1.py
@@ -0,0 +1,10 @@
+"Aula 4 - Guanabara"
+
+from xml.sax.handler import property_declaration_handler
+
+
+nome =  "Polyana" 
+idade = 18
+peso = 66
+
+print(nome,idade,peso)
